4282db507033467bf46cfee28a41ab2db6646f9d
Aditya Eka Pranata <aepranata@gmail.com>
2024-06-21 02:52:42 +0700

    atiga-priv: Prepare for AfterLife
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- keys/key.mk
+++
@@ -1,2 +0,0 @@
-$(warning "signing-build: Your build with signed with aepranata keys.")
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/atiga-priv/keys/releasekey
---
+++ keys/keys.mk
@@ -0,0 +1,2 @@
+$(warning "Your AfterLife build with signed with aepranata keys.")
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/after-priv/keys/releasekey
