3f01f26795c6ebbdb0c8eb172b4d33b99a260be2
Aditya Eka Pranata <aepranata@gmail.com>
2024-08-27 19:55:12 +0200

    Update path
    Change-Id: I960ace0e05a4f55ac8ec00e3de79cf92de0e25f5
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- keys/keys.mk
+++ keys/keys.mk
@@ -1,2 +1,3 @@
-$(warning "Your AfterLife build with signed with aepranata keys.")
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/after-priv/keys/releasekey
+$(warning "Your AfterlifeOS build with signed with aepranata keys.")
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/atiga-priv/keys/releasekey
+BUILD_KEYS := release-keys
