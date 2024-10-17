9ede13dc8de74db7473d5c318ece04cfd8b5f66d
Aditya Eka Pranata <aepranata@gmail.com>
2024-10-18 06:42:11 +0700

    keys: Move path to lieage-priv
    Change-Id: I9e3bcc43bae9c4478773517f391dcec2da07d658
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- keys/keys.mk
+++ keys/keys.mk
@@ -1,3 +1,3 @@
 $(warning "Your AfterlifeOS build with signed with aepranata keys.")
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/atiga-priv/keys/releasekey
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/lineage-priv/keys/releasekey
 BUILD_KEYS := release-keys
